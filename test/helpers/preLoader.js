2a209cb8b210e727cd492c2ad3b5806d386fa61a
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-08-04 15:41:47 +0300

    fix: add test

---
+++ test/helpers/preLoader.js
@@ -0,0 +1,42 @@
+import { RawSource } from 'webpack-sources';
+
+class PreCopyPlugin {
+  constructor(options = {}) {
+    this.options = options.options || {};
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  apply(compiler) {
+    const plugin = { name: 'PreCopyPlugin' };
+
+    compiler.hooks.compilation.tap(plugin, (compilation) => {
+      compilation.hooks.additionalAssets.tapAsync(plugin, (callback) => {
+        compilation.emitAsset(
+          'entry.css',
+          new RawSource('.entry {\n  text-align: center;\n}\n\n')
+        );
+
+        callback();
+      });
+    });
+  }
+}
+
+export default function loader() {
+  const callback = this.async();
+
+  const childCompiler = this._compilation.createChildCompiler(
+    `preloader`,
+    this.options
+  );
+
+  new PreCopyPlugin().apply(childCompiler);
+
+  childCompiler.runAsChild((error) => {
+    if (error) {
+      return callback(error);
+    }
+
+    return callback(null, 'export default 1');
+  });
+}
