1d58d748e018673928978b83e8696b8fdda513a0
evilebottnawi <sheo13666q@gmail.com>
2020-09-17 19:15:39 +0300

    test: cache

--- test/cache-option.test.js
+++ test/cache-option.test.js
@@ -417,12 +417,29 @@ if (getCompiler.isWebpack4()) {
       __dirname,
       './outputs/type-filesystem'
     );
+    const fileSystemCacheDirectory1 = path.resolve(
+      __dirname,
+      './outputs/type-filesystem-1'
+    );
+    const fileSystemCacheDirectory2 = path.resolve(
+      __dirname,
+      './outputs/type-filesystem-2'
+    );
+    const fileSystemCacheDirectory3 = path.resolve(
+      __dirname,
+      './outputs/type-filesystem-3'
+    );
 
     beforeAll(() => {
-      return Promise.all([del(fileSystemCacheDirectory)]);
+      return Promise.all([
+        del(fileSystemCacheDirectory),
+        del(fileSystemCacheDirectory1),
+        del(fileSystemCacheDirectory2),
+        del(fileSystemCacheDirectory3),
+      ]);
     });
 
-    it('should work with "false" value for the "cache" option', async () => {
+    it('should work with the "false" value for the "cache" option', async () => {
       const compiler = getCompiler({
         entry: {
           one: `${__dirname}/fixtures/cache.js`,
@@ -482,7 +499,7 @@ if (getCompiler.isWebpack4()) {
       expect(getWarnings(newStats)).toMatchSnapshot('warnings');
     });
 
-    it('should work with "memory" value for the "cache.type" option', async () => {
+    it('should work with the "memory" value for the "cache.type" option', async () => {
       const compiler = getCompiler({
         entry: {
           one: `${__dirname}/fixtures/cache.js`,
@@ -544,7 +561,7 @@ if (getCompiler.isWebpack4()) {
       expect(getWarnings(newStats)).toMatchSnapshot('warnings');
     });
 
-    it('should work with "filesystem" value for the "cache.type" option', async () => {
+    it('should work with the "filesystem" value for the "cache.type" option', async () => {
       const compiler = getCompiler({
         entry: {
           one: `${__dirname}/fixtures/cache.js`,
@@ -605,6 +622,169 @@ if (getCompiler.isWebpack4()) {
       expect(readAssets(compiler, newStats, '.css')).toMatchSnapshot('assets');
       expect(getErrors(newStats)).toMatchSnapshot('errors');
       expect(getWarnings(newStats)).toMatchSnapshot('warnings');
+    });
+
+    it('should work with the "filesystem" value for the "cache.type" option and source maps', async () => {
+      const compiler = getCompiler({
+        devtool: 'source-map',
+        entry: {
+          one: path.resolve(__dirname, './fixtures/cache.js'),
+          two: path.resolve(__dirname, './fixtures/cache-1.js'),
+          three: path.resolve(__dirname, './fixtures/cache-2.js'),
+          four: path.resolve(__dirname, './fixtures/cache-3.js'),
+          five: path.resolve(__dirname, './fixtures/cache-4.js'),
+        },
+        cache: {
+          type: 'filesystem',
+          cacheDirectory: fileSystemCacheDirectory1,
+        },
+      });
+
+      new CssMinimizerPlugin().apply(compiler);
+
+      let getCounter = 0;
+
+      compiler.cache.hooks.get.tap(
+        { name: 'TestCache', stage: -100 },
+        (identifier) => {
+          if (identifier.indexOf('CssMinimizerWebpackPlugin') !== -1) {
+            getCounter += 1;
+          }
+        }
+      );
+
+      let storeCounter = 0;
+
+      compiler.cache.hooks.store.tap(
+        { name: 'TestCache', stage: -100 },
+        (identifier) => {
+          if (identifier.indexOf('CssMinimizerWebpackPlugin') !== -1) {
+            storeCounter += 1;
+          }
+        }
+      );
+
+      const stats = await compile(compiler);
+
+      // Get cache for assets
+      expect(getCounter).toBe(5);
+      // Store cached assets
+      expect(storeCounter).toBe(5);
+      expect(readAssets(compiler, stats, '.css')).toMatchSnapshot('assets');
+      expect(getErrors(stats)).toMatchSnapshot('errors');
+      expect(getWarnings(stats)).toMatchSnapshot('warnings');
+
+      getCounter = 0;
+      storeCounter = 0;
+
+      const newStats = await compile(compiler);
+
+      // Get cache for assets
+      expect(getCounter).toBe(5);
+      // No need to store, we got cached assets
+      expect(storeCounter).toBe(0);
+      expect(readAssets(compiler, newStats, '.css')).toMatchSnapshot('assets');
+      expect(getErrors(newStats)).toMatchSnapshot('errors');
+      expect(getWarnings(newStats)).toMatchSnapshot('warnings');
+
+      await new Promise((resolve) => {
+        compiler.close(() => {
+          resolve();
+        });
+      });
+    });
+
+    it('should work with the "filesystem" value for the "cache.type" option and output warnings', async () => {
+      const compiler = getCompiler({
+        devtool: 'source-map',
+        entry: {
+          one: path.resolve(__dirname, './fixtures/cache.js'),
+          two: path.resolve(__dirname, './fixtures/cache-1.js'),
+          three: path.resolve(__dirname, './fixtures/cache-2.js'),
+          four: path.resolve(__dirname, './fixtures/cache-3.js'),
+          five: path.resolve(__dirname, './fixtures/cache-4.js'),
+        },
+        cache: {
+          type: 'filesystem',
+          cacheDirectory: fileSystemCacheDirectory2,
+        },
+      });
+
+      new CssMinimizerPlugin({
+        minify: (data) => {
+          // eslint-disable-next-line global-require
+          const postcss = require('postcss');
+          const [[fileName, input]] = Object.entries(data);
+
+          return postcss([
+            postcss.plugin('warning-plugin', () => (css, result) => {
+              result.warn(`Warning from ${result.opts.from}`, {
+                plugin: 'warning-plugin',
+              });
+            }),
+          ])
+            .process(input, { from: fileName, to: fileName })
+            .then((result) => {
+              return {
+                css: result.css,
+                map: result.map,
+                error: result.error,
+                warnings: result.warnings(),
+              };
+            });
+        },
+      }).apply(compiler);
+
+      let getCounter = 0;
+
+      compiler.cache.hooks.get.tap(
+        { name: 'TestCache', stage: -100 },
+        (identifier) => {
+          if (identifier.indexOf('CssMinimizerWebpackPlugin') !== -1) {
+            getCounter += 1;
+          }
+        }
+      );
+
+      let storeCounter = 0;
+
+      compiler.cache.hooks.store.tap(
+        { name: 'TestCache', stage: -100 },
+        (identifier) => {
+          if (identifier.indexOf('CssMinimizerWebpackPlugin') !== -1) {
+            storeCounter += 1;
+          }
+        }
+      );
+
+      const stats = await compile(compiler);
+
+      // Get cache for assets
+      expect(getCounter).toBe(5);
+      // Store cached assets
+      expect(storeCounter).toBe(5);
+      expect(readAssets(compiler, stats, '.css')).toMatchSnapshot('assets');
+      expect(getErrors(stats)).toMatchSnapshot('errors');
+      expect(getWarnings(stats)).toMatchSnapshot('warnings');
+
+      getCounter = 0;
+      storeCounter = 0;
+
+      const newStats = await compile(compiler);
+
+      // Get cache for assets
+      expect(getCounter).toBe(5);
+      // No need to store, we got cached assets
+      expect(storeCounter).toBe(0);
+      expect(readAssets(compiler, newStats, '.css')).toMatchSnapshot('assets');
+      expect(getErrors(newStats)).toMatchSnapshot('errors');
+      expect(getWarnings(newStats)).toMatchSnapshot('warnings');
+
+      await new Promise((resolve) => {
+        compiler.close(() => {
+          resolve();
+        });
+      });
     });
   });
 }
