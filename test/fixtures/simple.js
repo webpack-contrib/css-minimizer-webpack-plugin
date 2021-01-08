de8980f81ee057bf1de11ee00b6a6060fb153c58
evilebottnawi <sheo13666q@gmail.com>
2021-01-08 20:02:44 +0300

    chore(deps): update

--- test/fixtures/simple.js
+++ test/fixtures/simple.js
@@ -1,6 +1,6 @@
 import foo from './simple-emit';
 import bar from './simple-emit-2';
-import extracted from './foo.css';
+import './foo.css';
 
 async function load() {
   return import('./simple-async');
