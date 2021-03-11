d3c478434ec1ea22a4aefcddde7dfa447f35d730
cap-Bernardito <cap-Bernardito@yandex.ru>
2021-03-11 15:33:45 +0300

    feat: added support processorOptions

--- src/minify.js
+++ src/minify.js
@@ -1,11 +1,38 @@
 const cssnano = require('cssnano');
-
 /*
  * We bring to the line here, because when passing result from the worker,
  * the warning.toString is replaced with native Object.toString
  * */
 function warningsToString(warnings) {
   return warnings.map((i) => i.toString());
+}
+
+async function load(module) {
+  let exports;
+
+  try {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    exports = require(module);
+
+    return exports;
+  } catch (requireError) {
+    let importESM;
+
+    try {
+      // eslint-disable-next-line no-new-func
+      importESM = new Function('id', 'return import(id);');
+    } catch (e) {
+      importESM = null;
+    }
+
+    if (requireError.code === 'ERR_REQUIRE_ESM' && importESM) {
+      exports = await importESM(module);
+
+      return exports.default;
+    }
+
+    throw requireError;
+  }
 }
 
 const minify = async (options) => {
@@ -18,7 +45,48 @@ const minify = async (options) => {
     minify: minifyFn,
   } = options;
 
-  const postcssOptions = { to: name, from: name };
+  let postcssOptions;
+
+  if (minimizerOptions && minimizerOptions.processorOptions) {
+    postcssOptions = {
+      to: name,
+      from: name,
+      ...minimizerOptions.processorOptions,
+    };
+    delete minimizerOptions.processorOptions;
+  } else {
+    postcssOptions = { to: name, from: name };
+  }
+
+  if (typeof postcssOptions.parser === 'string') {
+    try {
+      postcssOptions.parser = await load(postcssOptions.parser);
+    } catch (error) {
+      throw new Error(
+        `Loading PostCSS "${postcssOptions.parser}" parser failed: ${error.message}\n\n(@${name})`
+      );
+    }
+  }
+
+  if (typeof postcssOptions.stringifier === 'string') {
+    try {
+      postcssOptions.stringifier = await load(postcssOptions.stringifier);
+    } catch (error) {
+      throw new Error(
+        `Loading PostCSS "${postcssOptions.stringifier}" stringifier failed: ${error.message}\n\n(@${name})`
+      );
+    }
+  }
+
+  if (typeof postcssOptions.syntax === 'string') {
+    try {
+      postcssOptions.syntax = await load(postcssOptions.syntax);
+    } catch (error) {
+      throw new Error(
+        `Loading PostCSS "${postcssOptions.syntax}" syntax failed: ${error.message}\n\n(@${name})`
+      );
+    }
+  }
 
   if (minifyFn) {
     const result = await minifyFn(
