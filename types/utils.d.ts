29f65a83451f04e31df118502ffe96073456e881
Sachin Kumar <ersachin3112@gmail.com>
2022-11-18 14:37:45 +0530

    chore: add cSpell to check spelling issues

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -11,7 +11,7 @@ export type Postcss = import("postcss").Postcss;
  * @typedef {() => Promise<T>} Task
  */
 /**
- * Run tasks with limited concurency.
+ * Run tasks with limited concurrency.
  * @template T
  * @param {number} limit - Limit of tasks that run at once.
  * @param {Task<T>[]} tasks - List of tasks to run.
