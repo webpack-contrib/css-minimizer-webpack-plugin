03c6274f166fea44506c21072f742f558b9a2bba
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2023-06-18 18:57:11 +0300

    refactor: code (#236)

--- src/utils.js
+++ src/utils.js
@@ -490,8 +490,34 @@ async function swcMinify(input, sourceMap, minimizerOptions) {
   };
 }
 
+/**
+ * @template T
+ * @param fn {(function(): any) | undefined}
+ * @returns {function(): T}
+ */
+function memoize(fn) {
+  let cache = false;
+  /** @type {T} */
+  let result;
+
+  return () => {
+    if (cache) {
+      return result;
+    }
+    result = /** @type {function(): any} */ (fn)();
+    cache = true;
+    // Allow to clean up memory for fn
+    // and all dependent resources
+    // eslint-disable-next-line no-undefined, no-param-reassign
+    fn = undefined;
+
+    return result;
+  };
+}
+
 module.exports = {
   throttleAll,
+  memoize,
   cssnanoMinify,
   cssoMinify,
   cleanCssMinify,
